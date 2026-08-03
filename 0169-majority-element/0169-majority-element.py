dece444ec2edc49daf22b613395623e9abae394c
Akshay Kumar <akshaykumarkolhar416@gmail.com>
2026-08-03 18:03:25 +0530

    Time: 16 ms (10.09%), Space: 21.1 MB (47.88%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        h={}
+        for i in nums:
+            if i in h:
+                h[i]+=1
+            else:
+                h[i]=1
+        return max(h,key=h.get)
