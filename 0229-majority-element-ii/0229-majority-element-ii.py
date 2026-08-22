c61a941831424098bb397069217913bb5bc245ba
Akshay Kumar <akshaykumarkolhar416@gmail.com>
2026-08-22 19:16:37 +0530

    Time: 9 ms (43.26%), Space: 22.5 MB (64.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,16 +1,40 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        comp=len(nums)//3
-        result={}
-        for i in nums:
-            if i in result:
-                result[i]+=1
+        c1=0
+        c2=1
+        cn1=0
+        cn2=0
+
+        for num in nums:
+            if num==c1:
+                cn1+=1
+            elif num==c2:
+                cn2+=1
+            elif cn1==0:
+                c1=num
+                cn1=1
+            elif cn2==0:
+                c2=num
+                cn2=1
             else:
-                result[i]=1
+                cn1-=1
+                cn2-=1
+            
+        cn1=0
+        cn2=0
 
-        final=[]
-        for i in result.keys():
-            if result[i]>comp:
-                final.append(i)
+        for i in nums:
+            if i ==c1:
+                cn1+=1
+            elif i==c2:
+                cn2+=1
+        
+        result=[]
+        n=len(nums)
 
-        return final 
+        if cn1>n//3:
+            result.append(c1)
+        if cn2>n//3:
+            result.append(c2)
+
+        return result 
